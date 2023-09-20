cc72d11c55441de33b9bf2c80e670cacadff4420
Alex Reis <108767586+AlexReisC@users.noreply.github.com>
2023-09-20 11:02:05 -0300

    Update sistema.h

--- sistema.h
+++ sistema.h
@@ -1,13 +1,28 @@
-struct passageiros{
+// dados individuais do passageiro (implementar função que cria passageiros e adiciona-los a uma lista ou adicionalos imediatamente a nave??)
+struct passageiro{
     char nome[30];
     int idade;
     char planeta[20];
     int id;
+    int doente; // atribui 1 caso esteja e 0 para indicar saudavel
 }
 
-struct recursos{
-    // ...
+// lista de passageiros?
+
+// armazena os passageiros de cada nave (implementar uma função para definir uma lista de passageiros para cada nave para assim identificar clandestinos???)
+struct passageiros{
+    passageiro p[10];
 }
+
+// compartimentos de recursos de cada nave (armazena três recursos aleatorios)
+struct compartimentosDerecursos{
+    char r1[20];
+    char r2[20];
+    char r3[20];
+}
+
+// lista dos 100 recursos disponiveis na galaxia (utilizar uma funçao para adiciona-los aleatoriamente aos compartimentos)
+char recursos[]
 
 typedef struct nave *Nave
 
